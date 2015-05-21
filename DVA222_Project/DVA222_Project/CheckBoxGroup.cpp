c54cf4b2d6bf20bb249eede8aae4d53de5a38c7d
jontelarsson94 <Airkick@99.87.243.130-v842.wireless.mdh.se>
2015-05-21 12:38:57 +0200

    ..

---
+++ DVA222_Project/DVA222_Project/CheckBoxGroup.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include "CheckBoxGroup.h"
+
+
+CheckBoxGroup::CheckBoxGroup()
+{
+}
+
+CheckBoxGroup::CheckBoxGroup(int x, int y, int w, int h)
+:Panel(x, y, w, h)
+{
+}
+
+void CheckBoxGroup::AddCheckBox()
+{
+
+}
+
+
+CheckBoxGroup::~CheckBoxGroup()
+{
+}
---
+++ DVA222_Project/DVA222_Project/CheckBoxGroup.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Panel.h"
+#include "CheckBox.h"
+using namespace std;
+class CheckBoxGroup :
+	public Panel
+{
+protected:
+	vector <CheckBox> boxes;
+public:
+
+	CheckBoxGroup();
+	CheckBoxGroup(int locX, int locY, int w, int h);
+	void AddCheckBox();
+	~CheckBoxGroup();
+};
+
